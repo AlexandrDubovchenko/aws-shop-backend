7ba302ab4cc16e425d04a23fc094b146679e0dfe
Oleksandr Dubovchenko <oleksandr_dubovchenko@epam.com>
2022-11-13 17:01:43 +0200

    Add auth

--- import-service/src/functions/importProductsFile/index.ts
+++ import-service/src/functions/importProductsFile/index.ts
@@ -15,6 +15,13 @@ export default {
             }
           }
         },
+        authorizer: {
+          name: 'basicAuthorizer',
+          arn: 'arn:aws:lambda:eu-west-1:473321033343:function:authorization-service-dev-basicAuthorizer',
+          resultTtlInSeconds: 0,
+          identitySource: 'method.request.header.Authorization',
+          type: 'token'
+        }
       },
     },
   ]
